bd95a360b78aafaec94c290c44f3fc67cc2a8bc5
Becervan <BenCer93@gmail.com>
2019-11-26 01:27:51 -0500

    Created getters/setters for OrderItem class.

--- src/core/OrderItem.java
+++ src/core/OrderItem.java
@@ -2,4 +2,34 @@ package core;
 
 public class OrderItem {
 	
+	private int id;
+	private String name;
+	private int quantity;
+	private double price;
+	private String notes;
+	
+	public OrderItem(int id, String name, int quantity, double price, String notes) {
+		
+		this.id = id; 
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price; 
+		this.notes = notes;
+		
+	}	
+	
+	// Getters
+	public int getId() {return id;}
+	public String getName() {return name;}
+	public int getQuantity() {return quantity;}
+	public double getPrice() {return price;}
+	public String getNotes() {return notes;}
+	
+	// Setters
+	public void setId(int id) {this.id = id;}
+	public void setName(String name) {this.name = name;}
+	public void setQuantity(int quantity) {this.quantity = quantity;}
+	public void setPrice(double price) {this.price = price;}
+	public void setNotes(String notes) {this.notes = notes;}
+	
 }
