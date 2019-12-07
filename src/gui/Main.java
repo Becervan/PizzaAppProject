fb026e22ab42ed3a5bf39559febe8d8d4a65ab58
PretzelJohn <58197328+PretzelJohn@users.noreply.github.com>
2019-12-06 22:06:36 -0500

    Added login screen

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -3,15 +3,13 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 
-//import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
-
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 //import javafx.scene.Cursor;
 //import javafx.scene.control.Label;
-
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 //import javafx.stage.Modality;
 //import javafx.stage.StageStyle;
@@ -22,24 +20,33 @@ public class Main extends Application {
 	private static final int SCREEN_HEIGHT = 900;
 	
 	private ClassLoader loader = getClass().getClassLoader();
-	private FXMLLoader fxmlLoader;
 	
 	//Loads and returns the root node from a FXML file
 	private VBox loadFXML(URL url) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader();
 		fxmlLoader.setLocation(url); //Gets the URL to the FXML file
 		return fxmlLoader.<VBox>load();
 	}
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		fxmlLoader = new FXMLLoader();
 		VBox root = loadFXML(loader.getResource("res/layout/main_screen.fxml"));
-		Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
+		VBox root2 = loadFXML(loader.getResource("res/layout/login_screen.fxml"));
+		
+		Stage stage2 = new Stage();
+		stage2.setTitle("The Oven - Login");
+		stage2.initOwner(stage);
+		stage2.initModality(Modality.APPLICATION_MODAL);
+		stage2.setFullScreen(false);
+		stage2.setScene(new Scene(root2, 2*SCREEN_WIDTH/3, 19*SCREEN_HEIGHT/20));
+		
 		
 		stage.setTitle(SCREEN_TITLE);
-		stage.setScene(scene);
 		stage.setFullScreen(true); //Sets the stage to fullscreen mode (stage must have a scene)
+		stage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
 		stage.show();
+		stage2.show();
+		
 	}
 	
 	//START HERE!
